22c608eb05a0b5edb486c649a513cb22b349659c
badmuffin <itsabhijeet06@gmail.com>
2025-03-10 11:03:30 +0530

    added lots of changes, level redesign, coins, kill zone, enemy, etc

---
+++ script/killzone.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var timer = $Timer # $Timer is a path, equivalent to root/Killzone/Timer
+
+func _on_body_entered(body):
+	print("you died!")
+	timer.start()
+
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
